8d5a533497b74919753dc9e760a447f701d726fd
Santiago <santiagoezequiellucero@gmail.com>
2021-04-26 08:58:28 -0400

    primer commit

---
+++ app/Http/Livewire/ShowHomes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ShowHomes extends Component
+{
+    public function render()
+    {
+        return view('livewire.show-homes');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\ShowHomes;
+
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', ShowHomes::class)->name('dashboard');
